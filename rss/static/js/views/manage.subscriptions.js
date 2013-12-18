a02f7e8bf4b0b78e9a2911cedfdd570214e1b340
lee <lee@apollo.(none)>
2013-12-17 19:59:08 -0500

    very basic structure to manage subscriptions

--- rss/static/js/views/manage.subscriptions.js
+++ rss/static/js/views/manage.subscriptions.js
@@ -22,8 +22,40 @@ var ManageSubscriptionsView = Backbone.View.extend({
 		
 	},
 
+	refreshItems : function ()
+	{
+		var element = $(this.el).find("#manage-grid");
+
+		var ref = this;
+		
+		$.ajax({
+			type: "POST",
+			url : "http://localhost:8000/api/get_subscriptions",
+			data : {
+				csrfmiddlewaretoken: getCSRF(),
+				user_id: window.get_userId()
+			},
+			beforeSend: function (){
+				
+			},
+			success: function (msg) {
+				var source   = $("#subscription-manage-node-template").html();
+				var template = Handlebars.compile(source);
+
+				var html = "";
+				for(var i = 0; i < msg.length; i ++)
+				{		
+					element.append(template(msg[i]));
+				}
+			}
+		});
+
+	},
+
 	render: function() {
 		$(this.el).html(this.template());
+
+		this.refreshItems();
 
 	    return this;
 	},
